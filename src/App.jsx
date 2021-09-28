5c0400ff4e04e3d726628f2d0a2730fe2d66e2c4
aycanogut <aycanonfire@gmail.com>
2021-09-28 16:36:47 +0300

    add styled components theme provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./Components/styled/Theme";
 import { Container } from "./Components/styled/Container.styled";
 import { Navbar } from "./Components/styled/Navbar.styled";
 
 function App() {
   return (
     <div className="App">
-      <Navbar>
-        <header>
-          <h1>Where in the world?</h1>
-        </header>
-        <div>
-          <img src="" alt="" />
-          <p>Dark Mode</p>
-        </div>
-      </Navbar>
-      <main>
-        <Container />
-      </main>
+      <ThemeProvider theme={theme}>
+        <Navbar>
+          <header>
+            <h1>Where in the world?</h1>
+          </header>
+          <div>
+            <img src="" alt="" />
+            <p>Dark Mode</p>
+          </div>
+        </Navbar>
+        <main>
+          <Container />
+        </main>
+      </ThemeProvider>
     </div>
   );
 }

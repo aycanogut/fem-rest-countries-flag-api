43637658485c20d143f543e35bde0b4adbcade4c
aycanogut <aycanonfire@gmail.com>
2021-09-28 16:36:56 +0300

    add navbar styles

--- src/Components/styled/Navbar.styled.js
+++ src/Components/styled/Navbar.styled.js
@@ -1,3 +1,14 @@
 import styled from "styled-components";
 
-export const Navbar = styled.nav``;
+export const Navbar = styled.nav`
+  background-color: ${({ theme }) => theme.colors.darkBlue};
+  font-weight: 400;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100px;
+
+  h1 {
+    color: var(--red);
+  }
+`;

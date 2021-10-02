43464b46cbdc6dc4b3b3eda53ec30ce9f580b698
aycanogut <aycanonfire@gmail.com>
2021-10-02 16:49:00 +0300

    add dark-mode implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components";
-import { theme } from "./Components/styled/Theme";
+import { GlobalStyles } from "./Components/styled/globalStyles";
+import { lightTheme, darkTheme, helpers } from "./Components/styled/Theme";
 import { Container } from "./Components/styled/Container.styled";
 import { Navbar } from "./Components/styled/Navbar.styled";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
   return (
     <div className="App">
-      <ThemeProvider theme={theme}>
-        <Navbar>
-          <header>
-            <h1>Where in the world?</h1>
-          </header>
-          <div>
-            <img src="" alt="" />
-            <p>Dark Mode</p>
-          </div>
-        </Navbar>
-        <main>
-          <Container />
-        </main>
+      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+        <React.Fragment>
+          <GlobalStyles />
+          <Navbar>
+            <header>
+              <h1>Where in the world?</h1>
+            </header>
+            <div>
+              <img src="" alt="" />
+              <button onClick={themeToggler}>Dark Mode</button>
+            </div>
+          </Navbar>
+          <main>
+            <Container />
+          </main>
+        </React.Fragment>
       </ThemeProvider>
     </div>
   );

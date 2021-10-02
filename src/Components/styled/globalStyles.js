20a40710a585fde570fbd782b918a4f2a55c5cae
aycanogut <aycanonfire@gmail.com>
2021-10-02 16:49:46 +0300

    add font-face

--- src/Components/styled/globalStyles.js
+++ src/Components/styled/globalStyles.js
@@ -3,14 +3,40 @@ import { theme } from "./Theme";
 
 export const GlobalStyles = createGlobalStyle`
 
-@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap');
+// fonts
+@font-face {
+    font-family: 'Nunito Sans';
+    src: url('src/assets/fonts/NunitoSans-SemiBold.woff2') format('woff2'),
+        url('src/assets/fonts/NunitoSans-SemiBold.woff') format('woff');
+    font-weight: 600;
+    font-style: normal;
+    font-display: swap;
+}
 
+@font-face {
+    font-family: 'Nunito Sans';
+    src: url('src/assets/fonts/NunitoSans-Light.woff2') format('woff2'),
+        url('src/assets/fonts/NunitoSans-Light.woff2') format('woff');
+    font-weight: 300;
+    font-style: normal;
+    font-display: swap;
+}
+
+@font-face {
+    font-family: 'Nunito Sans';
+    src: url('src/assets/fonts/NunitoSans-ExtraBold.woff2') format('woff2'),
+        url('src/assets/fonts/NunitoSans-ExtraBold.woff2') format('woff');
+    font-weight: bold;
+    font-style: normal;
+    font-display: swap;
+}
+
+// global styles
   body {
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
     font-family: 'Nunito Sans', sans-serif;
-    transition: all 0.50s linear;
+    transition: all 0.25s linear;
   }
-
 
 `;

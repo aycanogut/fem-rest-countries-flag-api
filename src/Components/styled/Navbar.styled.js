3fe56d23fdeeb4bfd9b263c7fdc35041a79707cd
aycanogut <aycanonfire@gmail.com>
2021-10-02 16:52:49 +0300

    update styles

--- src/Components/styled/Navbar.styled.js
+++ src/Components/styled/Navbar.styled.js
@@ -1,14 +1,8 @@
 import styled from "styled-components";
 
 export const Navbar = styled.nav`
-  background-color: ${({ theme }) => theme.colors.darkBlue};
-  font-weight: 400;
   display: flex;
   justify-content: space-between;
   align-items: center;
   height: 100px;
-
-  h1 {
-    color: var(--red);
-  }
 `;

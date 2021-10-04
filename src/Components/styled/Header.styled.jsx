053dfb610992d8eecea1c55c67b70b300e34c0b0
aycanogut <aycanonfire@gmail.com>
2021-10-04 20:04:52 +0300

    add header styles

--- src/Components/styled/Header.styled.jsx
+++ src/Components/styled/Header.styled.jsx
@@ -1,11 +1,14 @@
 import styled from "styled-components";
-import { Toggler } from "../Toggler";
+import { Toggler } from "./Toggler.styled";
 
 const StyledHeader = styled.header`
+  position: relative;
   display: flex;
   justify-content: space-between;
   align-items: center;
   height: 100px;
+  padding-inline: 2rem;
+  box-shadow: 0px -2px 8px 4px ${({ theme }) => theme.navShadow};
 `;
 
 export const Header = ({ children }) => {

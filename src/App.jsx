b416bb5156daeac21013b1ac1a0f80860f75eb1f
aycanogut <aycanonfire@gmail.com>
2021-10-04 16:37:09 +0300

    add mounted component state and conditional return

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,21 @@ import { useDarkMode } from "./Components/useDarkMode.js";
 import { lightTheme, darkTheme, helpers } from "./Components/Theme";
 import { Container } from "./Components/styled/Container.styled";
 import { Header } from "./Components/styled/Header.styled";
-import Toggler from "./Components/styled/Toggler";
+import { Toggler } from "./Components/styled/Toggler";
 
 const App = () => {
-  const [theme, setTheme] = useDarkMode();
+  const [theme, themeToggler, mountedComponent] = useDarkMode();
 
   const themeMode = theme === "light" ? lightTheme : darkTheme;
 
+  if (!mountedComponent) return <div />;
   return (
     <div className="App">
       <ThemeProvider theme={themeMode}>
         <React.Fragment>
           <GlobalStyles />
-          <Header>
-            <Toggler />
-            {props.children}
-          </Header>
+          <Header />
+          <Toggler theme={theme} toggleTheme={themeToggler} />
           <Container />
         </React.Fragment>
       </ThemeProvider>

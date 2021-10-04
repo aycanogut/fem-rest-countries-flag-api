4723cde5830defccb82dfc0493f1c4c569cac14e
aycanogut <aycanonfire@gmail.com>
2021-10-04 20:04:22 +0300

    add navbar shadow

--- src/Components/Theme.js
+++ src/Components/Theme.js
@@ -5,11 +5,13 @@ export const lightTheme = {
   text: "#111517",
   elements: "#fff",
   input: "#858585",
+  navShadow: "rgba(0,0,0,0.1)",
 };
 export const darkTheme = {
   body: "#202C37",
   text: "#fff",
   elements: "#2B3945",
+  navShadow: "rgba(0, 0, 0, 0.2)",
 };
 
 export const helpers = {

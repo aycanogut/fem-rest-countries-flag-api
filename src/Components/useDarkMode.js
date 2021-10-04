07fb1a33fac494d7dda57474d454c8c712f6a655
aycanogut <aycanonfire@gmail.com>
2021-10-04 16:36:49 +0300

    add mounted component state

--- src/Components/useDarkMode.js
+++ src/Components/useDarkMode.js
@@ -3,8 +3,10 @@ import { useEffect, useState } from "react";
 export const useDarkMode = () => {
   const [theme, setTheme] = useState("light");
 
+  const [mountedComponent, setMountedComponent] = useState(false);
+
   const setMode = (mode) => {
-    window.localStorage.setItem("theme, mode");
+    window.localStorage.setItem("theme", mode);
     setTheme(mode);
   };
 
@@ -14,8 +16,9 @@ export const useDarkMode = () => {
 
   useEffect(() => {
     const localTheme = window.localStorage.getItem("theme");
-    localTheme && setTheme(localTheme);
+    localTheme ? setTheme(localTheme) : setMode("light");
+    setMountedComponent(true);
   }, []);
 
-  return [theme, themeToggler];
+  return [theme, themeToggler, mountedComponent];
 };

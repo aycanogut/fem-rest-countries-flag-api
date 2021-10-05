21420156cddf7dbd65171bf208ff941a59b4c06f
aycanogut <aycanonfire@gmail.com>
2021-10-05 20:28:17 +0300

    remove bg color

--- src/Components/styled/Container.styled.jsx
+++ src/Components/styled/Container.styled.jsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 const StyledContainer = styled.div`
   max-width: 389px;
   margin: 0 auto;
-  background-color: red;
 `;
 
 export const Container = ({ children }) => {

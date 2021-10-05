0eb01710dabda227647c774c952cbb21b624e6c1
aycanogut <aycanonfire@gmail.com>
2021-10-05 19:32:13 +0300

    add conditional icon rendering

--- src/Components/styled/Toggler.styled.jsx
+++ src/Components/styled/Toggler.styled.jsx
@@ -1,32 +1,45 @@
 import React from "react";
 import { func, string } from "prop-types";
 import styled from "styled-components";
-import moonRegular from "../../assets/icons/moon-regular.svg";
-import moonSolid from "../../assets/icons/moon-solid.svg";
+import IconDark from "../../assets/icons/moon-half-dark.svg";
+import IconLight from "../../assets/icons/moon-half-light.svg";
 
 const Button = styled.button`
   background: none;
   color: ${({ theme }) => theme.text};
   outline: none;
-  border: none;
   cursor: pointer;
+  border: none;
   padding: 0;
   font-size: 1.6rem;
   font-weight: 600;
 `;
 
 const Icon = styled.img`
-  position: absolute;
-  right: 110px;
   width: 16px;
   height: 16px;
+`;
+
+const Wrapper = styled.div`
+  cursor: pointer;
 `;
 
 export const Toggler = ({ theme, toggleTheme }) => {
   return (
     <React.Fragment>
-      <Icon src={moonSolid} alt="dark mode moon icon" />
-      <Button onClick={toggleTheme}>Dark Mode</Button>
+      <Wrapper onClick={toggleTheme}>
+        <Icon
+          style={{ display: `${theme == "light" ? "none" : "inline"}` }}
+          src={IconDark}
+          alt="dark mode toggler"
+        />
+        <Icon
+          style={{ display: `${theme == "light" ? "inline" : "none"}` }}
+          src={IconLight}
+          alt="light mode toggler"
+        />
+        <Button>{theme === "light" ? "Dark Mode" : "Light Mode"}</Button>
+      </Wrapper>
     </React.Fragment>
   );
 };

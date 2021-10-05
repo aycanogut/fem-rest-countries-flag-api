808710168a59d4d1af9dbe63da1b380662d056df
aycanogut <aycanonfire@gmail.com>
2021-10-05 20:20:25 +0300

    remove helpers

--- src/Components/Theme.js
+++ src/Components/Theme.js
@@ -1,5 +1,7 @@
 import { ThemeProvider } from "styled-components";
 
+export const theme = {};
+
 export const lightTheme = {
   body: "#FAFAFA",
   text: "#111517",
@@ -7,24 +9,10 @@ export const lightTheme = {
   input: "#858585",
   navShadow: "rgba(0,0,0,0.1)",
 };
+
 export const darkTheme = {
   body: "#202C37",
   text: "#fff",
   elements: "#2B3945",
   navShadow: "rgba(0, 0, 0, 0.2)",
 };
-
-export const helpers = {
-  breakpoints: {
-    sm: `320px`,
-    md: "768px",
-    lg: "1200px",
-  },
-  fontWeight: {
-    thin: 300,
-    semiBold: 600,
-    extraBold: 800,
-  },
-};
-
-export const theme = {};

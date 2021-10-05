bd9099f564bf84ad41970f5dad8eeceeeaab856b
aycanogut <aycanonfire@gmail.com>
2021-10-05 20:21:17 +0300

    remove helpers import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "./Components/globalStyles";
 import { useDarkMode } from "./Components/useDarkMode.js";
-import { lightTheme, darkTheme, helpers } from "./Components/Theme";
+import { lightTheme, darkTheme } from "./Components/Theme";
 import { Container } from "./Components/styled/Container.styled";
 import { Header } from "./Components/styled/Header.styled";
 import { Toggler } from "./Components/styled/Toggler.styled";

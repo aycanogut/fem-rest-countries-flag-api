a7f8c02d747f469e3f9e9e3b0160caea26ced7d5
aycanogut <aycanonfire@gmail.com>
2021-10-06 17:58:13 +0300

    add responsive header inline padding

--- src/Components/styled/Header.styled.jsx
+++ src/Components/styled/Header.styled.jsx
@@ -8,11 +8,22 @@ const StyledHeader = styled.header`
   justify-content: space-between;
   align-items: center;
   height: 100px;
-  padding-inline: 1.2rem;
+  padding: 0 1.2rem;
+
   box-shadow: 0px -2px 8px 4px ${({ theme }) => theme.navShadow};
 
   @media only screen and ${helpers.device.sm} {
-    padding-inline: 2rem;
+    padding: 0 1.6rem 0 2rem;
+  }
+
+  @media only screen and ${helpers.device.md} {
+    height: 160px;
+    padding: 0 3rem 0 3.2rem;
+  }
+
+  @media only screen and ${helpers.device.lg} {
+    height: 80px;
+    padding: 0 7.6rem 0 08rem;
   } ;
 `;
 

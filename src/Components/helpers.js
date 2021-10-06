3d3a4c199bcd1f3832a1cc6ddf6b58db4fff5d68
aycanogut <aycanonfire@gmail.com>
2021-10-06 17:58:42 +0300

    update breakpoint sizes

--- src/Components/helpers.js
+++ src/Components/helpers.js
@@ -1,14 +1,15 @@
 // breakpoints
 const size = {
   xs: "320px",
-  sm: "375px",
-  md: "768px",
+  sm: "465px",
+  md: "750px",
   lg: "1200px",
 };
 
 const device = {
   xs: `(min-width: ${size.xs})`,
   sm: `(min-width: ${size.sm})`,
+  md: `(min-width: ${size.md})`,
   lg: `(min-width: ${size.lg})`,
 };
 

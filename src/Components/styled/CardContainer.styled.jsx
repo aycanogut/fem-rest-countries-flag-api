48a22bdeb47ab66d5b482d635bd6cbfdc22a1b52
aycanogut <aycanonfire@gmail.com>
2021-12-14 15:19:06 +0300

    add responsive spacing

--- src/Components/styled/CardContainer.styled.jsx
+++ src/Components/styled/CardContainer.styled.jsx
@@ -11,7 +11,7 @@ import Card from "./Card.styled";
 const StyledContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr;
-  row-gap: 3rem;
+  row-gap: 5rem;
   max-width: 280px;
   margin: 50px auto; //! update
   background-color: ${({ theme }) => theme.body};
@@ -23,16 +23,20 @@ const StyledContainer = styled.div`
 
   @media only screen and ${helpers.device.md} {
     grid-template-columns: 1fr 1fr;
+    column-gap: 1rem;
     max-width: 640px;
   }
 
   @media only screen and ${helpers.device.lg} {
     grid-template-columns: 1fr 1fr 1fr;
+    column-gap: 5rem;
     max-width: 864px;
   }
 
   @media only screen and ${helpers.device.xl} {
     grid-template-columns: 1fr 1fr 1fr 1fr;
+    row-gap: 7.5rem;
+    column-gap: 7.5rem;
     max-width: 1280px;
   }
 `;

1534004fe491a1a4791d080d46510c336c8ea026
aycanogut <aycanonfire@gmail.com>
2021-12-14 15:19:33 +0300

    add responsive styling for fonts, size and spacing

--- src/Components/styled/Card.styled.jsx
+++ src/Components/styled/Card.styled.jsx
@@ -10,36 +10,67 @@ const StyledWrapper = styled.article`
   flex-direction: column;
   background-color: ${({ theme }) => theme.elements};
   box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
-
   border-bottom-left-radius: 0.6rem;
   border-bottom-right-radius: 0.6rem;
+
+  @media only screen and ${helpers.device.md} {
+    max-width: 264px;
+  } ;
 `;
 
 const StyledImgWrapper = styled.div`
-  width: 328px;
-  height: 200px;
+  max-width: 100%;
+  height: auto;
 `;
 
 const StyledImage = styled.img`
   width: 100%;
-  height: 100%;
+  height: 180px;
   object-fit: cover;
   border-top-left-radius: 0.6rem;
   border-top-right-radius: 0.6rem;
+
+  @media only screen and ${helpers.device.sm} {
+    width: 328px;
+    height: 200px;
+  }
+
+  @media only screen and ${helpers.device.md} {
+    width: 265px;
+    height: 160px;
+  }
 `;
 
 const StyledCardBottom = styled.div`
-  padding: 3.5rem 3.1rem 4.4rem 3.1rem;
+  padding: 3rem 3rem 4.3rem 3rem;
+
+  @media only screen and ${helpers.device.md} {
+    padding: 2.3rem 2.4rem 3.1rem 2.5rem;
+  } ;
 `;
+
 const StyledTitle = styled.header`
   font-size: 2.2rem;
   font-weight: ${helpers.fontWeight.extraBold};
+  padding: 0 0 0.7rem 0;
+
+  @media only screen and ${helpers.device.md} {
+    font-size: 1.7rem;
+  }
 `;
+
 const StyledList = styled.ul``;
+
 const StyledListNode = styled.li`
-  padding: 0.3rem 0;
   font-size: 1.75rem;
+  line-height: 2.8rem;
+
+  @media only screen and ${helpers.device.md} {
+    font-size: 1.4rem;
+    line-height: 2.4rem;
+  }
 `;
+
 const StyledSpan = styled.span`
   font-weight: ${helpers.fontWeight.semiBold};
 `;

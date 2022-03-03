0fcc6302facde36acd6af3664fef52b25e17c689
Aycan Ogut <aycanonfire@gmail.com>
2022-03-03 22:34:14 +0300

    chore: :wrench: update configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,21 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
+{
+  "env": {
+    "browser": true,
+    "es2021": true
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+  "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:prettier/recommended"],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
     },
-    ecmaVersion: 13,
-    sourceType: "module",
+    "ecmaVersion": "latest",
+    "sourceType": "module"
   },
-  plugins: ["react"],
-  rules: {
+  "plugins": ["react"],
+  "rules": {
     "react/react-in-jsx-scope": "off",
-  },
-};
+    "react/prop-types": 0,
+    "no-undef": "off",
+    "no-unused-vars": "off"
+  }
+}

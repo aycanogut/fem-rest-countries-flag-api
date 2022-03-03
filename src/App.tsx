62e17cdb6e72da0a5394427fd8454fcec0aa413a
Aycan Ogut <aycanonfire@gmail.com>
2022-03-03 22:01:46 +0300

    refactor: :recycle: update extension

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,10 @@ import { Toggler } from "./Components/styled/Toggler.styled";
 const App = () => {
   const [theme, themeToggler, mountedComponent] = useDarkMode();
 
+  console.log(theme);
+  // console.log(themeToggler);
+  // console.log(mountedComponent);
+
   const themeMode = theme === "light" ? lightTheme : darkTheme;
 
   if (!mountedComponent) return <div />;

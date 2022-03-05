4c43d1356897479f5f278ff1c0f2bad7fad822fc
Aycan Ogut <aycanonfire@gmail.com>
2022-03-05 14:04:08 +0300

    feat: :art: add hooks, style files and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import { ThemeProvider } from 'styled-components'
+
+import useCountries from './hooks/useCountries'
+import useDarkMode from './hooks/useDarkMode'
+
+import { Header } from './Components/Header.styled'
+
 import { GlobalStyles } from './styles/globalStyles'
-import { useDarkMode } from './hooks/useDarkMode'
 import { lightTheme, darkTheme } from './styles/theme'
 
 function App() {
+  const [theme, themeToggler, mountedComponent] = useDarkMode()
+
+  const themeMode: any = theme === 'light' ? lightTheme : darkTheme
+
+  if (!mountedComponent) return <div />
+
   return (
     <div className="App">
-      <div>test</div>
+      <ThemeProvider theme={themeMode}>
+        <>
+          <GlobalStyles />
+          <Header>
+            <h1>Where in the world?</h1>
+            {/* <Toggler theme={theme} toggleTheme={themeToggler} /> */}
+          </Header>
+        </>
+      </ThemeProvider>
     </div>
   )
 }

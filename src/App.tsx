79a57dc7e141b970a187a9ac2e8dab8207b62d58
Aycan Ogut <aycanonfire@gmail.com>
2022-03-13 15:56:38 +0300

    feat: :heavy_plus_sign: add browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import React, { useState, useEffect } from 'react'
-import { ThemeProvider } from 'styled-components'
-
-import useCountries from './hooks/useCountries'
-import useDarkMode from './hooks/useDarkMode'
-
-import Header from './Components/Header.styled'
-import Toggler from './Components/Toggler.styled'
-import CardContainer from './Components/CardContainer.styled'
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
 import { GlobalStyles } from './styles/globalStyles'
-import { lightTheme, darkTheme } from './styles/theme'
+
+import Wrapper from './Components/Wrapper'
 
 function App() {
-  const [theme, themeToggler, mountedComponent] = useDarkMode()
-
-  const themeMode: any = theme === 'light' ? lightTheme : darkTheme
-
-  if (!mountedComponent) return <div />
-
   return (
     <div className="App">
-      <ThemeProvider theme={themeMode}>
-        <>
-          <GlobalStyles />
-          <Header>
-            <h1>Where in the world?</h1>
-            <Toggler theme={theme} toggleTheme={themeToggler} />
-          </Header>
-          <CardContainer />
-        </>
-      </ThemeProvider>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Wrapper />
+      </BrowserRouter>
     </div>
   )
 }

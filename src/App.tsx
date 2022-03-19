f4a8faef508475ab3b2a37b6d7bf5e7842a22c30
Aycan Ogut <aycanonfire@gmail.com>
2022-03-19 20:31:54 +0300

    refactor: :recycle: move content to wrapper and add interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+
+import useDarkMode from './hooks/useDarkMode'
 
 import { GlobalStyles } from './styles/globalStyles'
+import { lightTheme, darkTheme } from './styles/theme'
 
 import Wrapper from './Components/Wrapper'
 
-function App() {
+const App = () => {
+  const [theme, themeToggler, mountedComponent] = useDarkMode()
+
+  const themeMode: any = theme === 'light' ? lightTheme : darkTheme
+
+  if (!mountedComponent) return <div />
+
   return (
     <div className="App">
-      <GlobalStyles />
-      <BrowserRouter>
-        <Wrapper />
-      </BrowserRouter>
+      <ThemeProvider theme={themeMode}>
+        <GlobalStyles />
+        <BrowserRouter>
+          <Wrapper theme={theme} themeToggler={themeToggler} />
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   )
 }
--- src/Components/Wrapper.tsx
+++ src/Components/Wrapper.tsx
@@ -1,30 +1,23 @@
 import React from 'react'
-import { ThemeProvider } from 'styled-components'
-
-import useCountries from '../hooks/useCountries'
-import useDarkMode from '../hooks/useDarkMode'
-
-import { lightTheme, darkTheme } from '../styles/theme'
 
 import Header from './Header'
 import Toggler from './Toggler'
 import CardContainer from './CardContainer'
 
-const Wrapper: React.FC = () => {
-  const [theme, themeToggler, mountedComponent] = useDarkMode()
+interface IWrapperProps {
+  theme: string | boolean | (() => void)
+  themeToggler: string | boolean | (() => void)
+}
 
-  const themeMode: any = theme === 'light' ? lightTheme : darkTheme
-
-  if (!mountedComponent) return <div />
-
+const Wrapper: React.FC<IWrapperProps> = ({ theme, themeToggler }) => {
   return (
-    <ThemeProvider theme={themeMode}>
+    <main>
       <Header>
         <h1>Where in the world?</h1>
         <Toggler theme={theme} toggleTheme={themeToggler} />
       </Header>
       <CardContainer />
-    </ThemeProvider>
+    </main>
   )
 }
 

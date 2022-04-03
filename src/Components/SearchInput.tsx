879c84f08f575e2fd4cf90fc3991f5b0a65ca72d
Aycan Ogut <aycanonfire@gmail.com>
2022-04-03 17:08:32 +0300

    feat: :sparkles: add styles

--- src/Components/SearchInput.tsx
+++ src/Components/SearchInput.tsx
@@ -1,11 +1,14 @@
 import React, { ChangeEvent } from 'react'
+import styled from 'styled-components'
+
+const StyledInput = styled.input``
 
 interface ISearchInput {
   onChange: (event: ChangeEvent<HTMLInputElement>) => void
 }
 
 const SearchInput: React.FC<ISearchInput> = ({ onChange }) => {
-  return <input type="text" onChange={onChange} />
+  return <StyledInput type="text" onChange={onChange} />
 }
 
 export default SearchInput

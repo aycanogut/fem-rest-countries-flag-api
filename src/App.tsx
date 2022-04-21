385d1de9b078f3e2b085937bf439505918d0ebdd
Aycan Ogut <aycanonfire@gmail.com>
2022-04-21 14:43:36 +0300

    feat: :sparkles: add react-router and update structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import React, { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
 import useDarkMode from './hooks/useDarkMode'
@@ -7,9 +7,12 @@ import useDarkMode from './hooks/useDarkMode'
 import { GlobalStyles } from './styles/globalStyles'
 import { lightTheme, darkTheme } from './styles/theme'
 
+import Header from './Components/Header'
 import Wrapper from './Components/Wrapper'
+import CardDetail from './Components/CardDetail'
 
-const App = () => {
+const App: React.FC = () => {
+  const [searchTerm, setSearchTerm] = useState('')
   const [theme, themeToggler, mountedComponent] = useDarkMode()
 
   const themeMode: any = theme === 'light' ? lightTheme : darkTheme
@@ -21,7 +24,14 @@ const App = () => {
       <ThemeProvider theme={themeMode}>
         <GlobalStyles />
         <BrowserRouter>
-          <Wrapper theme={theme} themeToggler={themeToggler} />
+          <Header theme={theme} themeToggler={themeToggler} />
+          <Routes>
+            <Route
+              path="/"
+              element={<Wrapper searchTerm={searchTerm} setSearchTerm={setSearchTerm} />}
+            />
+            <Route path=":id" element={<CardDetail />} />
+          </Routes>
         </BrowserRouter>
       </ThemeProvider>
     </div>

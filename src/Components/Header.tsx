305714363591ee7e51c26a37b1ee4bca671a55de
Aycan Ogut <aycanonfire@gmail.com>
2022-11-10 12:52:42 +0300

    fix: :lipstick: fix header spacing && add wrapper

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -6,28 +6,33 @@ import helpers from '../styles/helpers'
 
 import Toggler from './Toggler'
 
+const StyledHeaderWrapper = styled.div`
+  box-shadow: 0px -2px 8px 4px ${({ theme }) => theme.shadow};
+`
+
 const StyledHeader = styled.header`
   position: relative;
   display: flex;
   justify-content: space-between;
   align-items: center;
   height: 100px;
-  padding: 0 1.2rem;
-  box-shadow: 0px -2px 8px 4px ${({ theme }) => theme.shadow};
-
-  @media only screen and ${helpers.device.sm} {
-    padding: 0 1.6rem 0 2rem;
-  }
+  margin: 0 auto;
+  padding: 0 2rem;
 
   @media only screen and ${helpers.device.md} {
+    max-width: 590px;
     height: 160px;
-    padding: 0 3rem 0 3.2rem;
+    padding: 0;
   }
 
   @media only screen and ${helpers.device.lg} {
+    max-width: 868px;
     height: 80px;
-    padding: 0 7.6rem 0 08rem;
-  } ;
+  }
+
+  @media only screen and ${helpers.device.xl} {
+    max-width: 1280px;
+  }
 `
 
 interface IHeaderProps {
@@ -37,12 +42,14 @@ interface IHeaderProps {
 
 const Header: React.FC<IHeaderProps> = ({ theme, themeToggler }) => {
   return (
-    <StyledHeader>
-      <Link to="/">
-        <h1>Where in the world?</h1>
-      </Link>
-      <Toggler theme={theme} toggleTheme={themeToggler} />
-    </StyledHeader>
+    <StyledHeaderWrapper>
+      <StyledHeader>
+        <Link to="/">
+          <h1>Where in the world?</h1>
+        </Link>
+        <Toggler theme={theme} toggleTheme={themeToggler} />
+      </StyledHeader>
+    </StyledHeaderWrapper>
   )
 }
 
